00b20bd4fb4181e075066a171c88f178bb16be8b
UMB-Linus <linus.laeubli@umb.ch>
2018-06-29 16:02:26 +0200

    strip excess code

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -20,16 +20,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
         {
             var message = await argument;
 
-            if (message.Text == "reset")
-            {
-                PromptDialog.Confirm(
-                    context,
-                    AfterResetAsync,
-                    "Are you sure you want to reset the count? Confirm with 'Yes'",
-                    "Didn't get that!",
-                    promptStyle: PromptStyle.Auto);
-            }
-            else if (message.Text == "help")
+            if (message.Text == "help")
             {
                 await context.PostAsync(
                     "The following commands are supported:\n"
@@ -39,7 +30,7 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             else if (message.Text.ToLower().Contains("health"))
             {
                 await context.PostAsync(
-                    "Sorry, can't do that yet :(");
+                    "What do you want?");
                     //await context.Forward(new NewOrderDialog(), this.ResumeAfterNewOrderDialog, message, CancellationToken.None);
             }
 
@@ -50,20 +41,6 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
         }
 
-        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
-        {
-            var confirm = await argument;
-            if (confirm)
-            {
-                this.count = 1;
-                await context.PostAsync("Reset count");
-            }
-            else
-            {
-                await context.PostAsync("Did not reset count.");
-            }
-            context.Wait(MessageReceivedAsync);
-        }
 
     }
 }
