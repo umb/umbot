48017e326b47b75259be87c8bbd9dad872c0426d
UMB-Linus <linus.laeubli@umb.ch>
2018-06-29 15:51:15 +0200

    namespace change

--- Dialogs/EchoDialog.cs
+++ Dialogs/EchoDialog.cs
@@ -5,7 +5,7 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Prompts;
 using System.Net.Http;
 
-namespace Microsoft.Bot.Sample.SimpleEchoBot
+namespace UMBot
 {
     [Serializable]
     public class EchoDialog : IDialog<object>
@@ -30,53 +30,8 @@ namespace Microsoft.Bot.Sample.SimpleEchoBot
             }
             else if (message.Text.ToLower().Contains("health"))
             {
-                dialogs = new DialogSet();
-
-                // Define our dialog
-                dialogs.Add("reserveTable", new WaterfallStep[]
-                {
-                    async (dc, args, next) =>
-                    {
-                        // Prompt for the guest's name.
-                        await dc.Context.SendActivity("Welcome to the reservation service.");
-
-                        await dc.Prompt("dateTimePrompt", "Please provide a reservation date and time.");
-                    },
-                    async(dc, args, next) =>
-                    {
-                        var dateTimeResult = ((DateTimeResult)args).Resolution.First();
-
-                        reservationDate = Convert.ToDateTime(dateTimeResult.Value);
-                        
-                        // Ask for next info
-                        await dc.Prompt("partySizePrompt", "How many people are in your party?");
-
-                    },
-                    async(dc, args, next) =>
-                    {
-                        partySize = (int)args["Value"];
-
-                        // Ask for next info
-                        await dc.Prompt("textPrompt", "Whose name will this be under?");
-                    },
-                    async(dc, args, next) =>
-                    {
-                        reservationName = args["Text"];
-                        string msg = "Reservation confirmed. Reservation details - " +
-                        $"\nDate/Time: {reservationDate.ToString()} " +
-                        $"\nParty size: {partySize.ToString()} " +
-                        $"\nReservation name: {reservationName}";
-                        await dc.Context.SendActivity(msg);
-                        await dc.End();
-                    }
-                });
-
-                // Add a prompt for the reservation date
-                dialogs.Add("dateTimePrompt", new Microsoft.Bot.Builder.Dialogs.DateTimePrompt(Culture.English));
-                // Add a prompt for the party size
-                dialogs.Add("partySizePrompt", new Microsoft.Bot.Builder.Dialogs.NumberPrompt<int>(Culture.English));
-                // Add a prompt for the user's name
-                dialogs.Add("textPrompt", new Microsoft.Bot.Builder.Dialogs.TextPrompt());
+                await context.PostAsync(
+                    "Blablabla");
             }
             else
             {
