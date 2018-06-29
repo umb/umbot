ec71cfc2113069e9dc1a56e31d885dca00320789
UMB-Linus <linus.laeubli@umb.ch>
2018-06-29 13:42:57 +0200

    Original Comit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=um-bot-Web-Deploy.pubxml -p:Password=zJNd7Gpt026HfsfP9MNrfEBCFR4Tyo6dupdMHjReurSorunbhMC0PgzYvntr
+
