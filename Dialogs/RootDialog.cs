ac3c36a04efc918233b27d97085e233d2628f95d
UMB-Linus <linus.laeubli@umb.ch>
2018-06-30 15:02:12 +0200

    test

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -234,8 +234,8 @@
             using (var httpClient = new HttpClient())
             {
                 httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
-                //var html = httpClient.GetStringAsync(new Uri(url)).Result;
-                var response = httpClient.PostAsync(url, testJ).Result;
+                var response = httpClient.GetStringAsync(new Uri(url)).Result;
+                //var response = httpClient.PostAsync(url, testJ).Result;
                 await context.PostAsync($"Response: {response}");
             }
  
